1ab4e0dc65c06040bcfbc142d96dfc75e6252c69
InvisiBug <matthewjkavanagh@gmail.com>
2022-05-11 14:31:07 +0100

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const IndexPage = () => {
+  return (
+    <>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default IndexPage;
