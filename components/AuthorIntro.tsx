536806f5910832a2f0bfde7b02489e97309a8dab
InvisiBug <matthewjkavanagh@gmail.com>
2022-06-10 12:22:13 +0100

    Starting sanity stuff

---
+++ components/AuthorIntro.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
+
+const AuthorInfo: FC<any> = () => {
+  return (
+    <>
+      <Row>
+        <Col md="8">
+          <Card className="mb-4 admin-intro">
+            <Image
+              roundedCircle
+              width={64}
+              height={64}
+              className="mr-3"
+              src="https://avatars1.githubusercontent.com/u/9482724?s=460&u=69a6acab13fd5547a4e316e496b573271077147f&v=4"
+              alt="Generic placeholder"
+            />
+            <Card.Body>
+              <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
+              <p className="welcome-text">
+                My name is Filip Jerga and I am an experienced software engineer and freelance developer. and this is my blog page.
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default AuthorInfo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
-import Link from "next/link";
+import { Row, Col } from "react-bootstrap";
+import Layout from "components/Layout";
+import AuthorInfo from "components/AuthorIntro";
+import LargeCard from "components/LargeCard";
+import SmallCard from "components/SmallCard";
 
 const IndexPage = () => {
   return (
-    <>
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </>
+    <div>
+      <Layout>
+        <hr />
+        <AuthorInfo />
+
+        <Row className="mb-5">
+          <Col md="10">
+            <LargeCard />
+          </Col>
+
+          <Col md="4">
+            <SmallCard />
+          </Col>
+        </Row>
+      </Layout>
+    </div>
   );
 };
 
